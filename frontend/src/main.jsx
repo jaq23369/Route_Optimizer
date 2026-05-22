80c9d65d6f04581a8d9c96875c061c0710fcbecd
Zacatac23 <zac231104@uvg.edu.gt>
2026-05-22 13:05:31 -0600

    [ADD] Setup main entry point and React bootstrapping

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,2 +1,10 @@
-// TODO: Main app entry point
-// import App from './App';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
